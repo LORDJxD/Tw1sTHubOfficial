5a2b1825aeefa3685b06b48f3f0ac5f352819954
LORD JxD <JoshXDanz52@gmail.com>
2025-12-06 16:50:05 +0800

    Add files via upload

---
+++ Tw1sT/nav/navbar.js
@@ -0,0 +1,7 @@
+// Dynamically load the navbar
+fetch('nav/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
